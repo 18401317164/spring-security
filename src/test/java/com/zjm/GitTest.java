77a5782825d1bbbf097acc13e5f0847df91d7685
张佳敏 <zhangjiamin@kurumi.com.cn>
2022-07-31 21:17:17 +0800

    git merge test

---
+++ src/test/java/com/zjm/GitTest.java
@@ -0,0 +1,12 @@
+package com.zjm;
+
+/**
+ * @author 张佳敏
+ * @Description
+ * @create 2022/7/31 21:15
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test merge");
+    }
+}
